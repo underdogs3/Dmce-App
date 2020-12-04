830bc985e40f09bfa8a31167ec1218f0f697500f
underdogs3 <73481519+underdogs3@users.noreply.github.com>
2020-12-04 22:03:26 +0530

    Profile updated

--- app/src/main/java/com/example/loginpage/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/loginpage/Fragment/ProfileFragment.java
@@ -1,66 +1,85 @@
 package com.example.loginpage.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.loginpage.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment () {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance (String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        String[] ptitle = {
+                "First Name",
+                "Last Name",
+                "Date of Birth",
+                "Email id",
+                "Phone",
+                "Father Name",
+                "Mother Name"
+        };
+
+        String[] psubtitle = {
+                "xyz",
+                "xyz",
+                "DD/MM/YYY",
+                "xyz@",
+                "123456789",
+                "xyz",
+                "xyz",
+        };
+
+        ListView listView = view.findViewById(R.id.list2);
+        MyAdapter adapter = new MyAdapter(getActivity(),ptitle,psubtitle);
+        listView.setAdapter(adapter);
+
+                return view;
     }
+
+        static class MyAdapter extends ArrayAdapter<String>{
+
+        Context context;
+        String[] rtitle;
+        String[] rsubtitle;
+
+        MyAdapter(Context c,String[] title, String[] subtitle){
+            super(c,R.layout.profilelist,R.id.titletxt,title);
+            this.context = c;
+            this.rtitle = title;
+            this.rsubtitle = subtitle;
+        }
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                @SuppressLint("ViewHolder") View profilelist = layoutInflater.inflate(R.layout.profilelist, parent, false);
+
+                TextView myTitle = profilelist.findViewById(R.id.titletxt);
+                TextView mysubtitle = profilelist.findViewById(R.id.subtitletxt);
+
+                myTitle.setText(rtitle[position]);
+                mysubtitle.setText(rsubtitle[position]);
+
+                return profilelist;
+            }
+        }
 }
