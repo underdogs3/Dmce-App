1089ae6dcfd709cae247993229a1325a3ca1827d
Samaarrr <74596196+Samaarrr@users.noreply.github.com>
2021-05-09 20:45:21 +0530

    profile

--- app/src/main/java/com/example/loginpage/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/loginpage/Fragment/ProfileFragment.java
@@ -8,14 +8,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.loginpage.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -53,22 +57,48 @@ public class ProfileFragment extends Fragment {
         userId = fAuth.getCurrentUser().getUid();
 
         DocumentReference documentReference = fStore.collection("students").document(userId);
-        documentReference.addSnapshotListener((Executor) this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                name.setText(value.getString("name"));
-                gr_number.setText(value.getString("gr_no"));
-                year.setText(value.getString("year"));
-                phone.setText(value.getString("phone_no"));
-                sem.setText(value.getString("sem"));
-                email.setText(value.getString("email"));
-                dob.setText(value.getString("dob"));
-                attendence.setText(value.getString("attendance"));
+        documentReference.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess (DocumentSnapshot value) {
+                        if(value.exists()){
+                            name.setText(value.getString("name"));
+                            gr_number.setText(value.getString("gr_no"));
+                            year.setText(value.getString("year"));
+                            phone.setText(value.getString("phone_no"));
+                            sem.setText(value.getString("sem"));
+                            email.setText(value.getString("email"));
+                           dob.setText(value.getString("dob"));
+                            attendence.setText(value.getString("attendance"));
+                        }else{
 
-            }
-        });
+                            Toast.makeText(getContext(), "Document does not exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure (@NonNull Exception e) {
+                        Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.d("TAG",e.toString());
+                    }
+                });
+//        documentReference.addSnapshotListener((Executor) this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+//                name.setText(documentSnapshot.getString("name"));
+//                gr_number.setText(value.getString("gr_no"));
+//                year.setText(value.getString("year"));
+//                phone.setText(value.getString("phone_no"));
+//                sem.setText(value.getString("sem"));
+//                email.setText(value.getString("email"));
+//                dob.setText(value.getString("dob"));
+//                attendence.setText(value.getString("attendance"));
+
+
 
 
         return view;
+
     }
 }
